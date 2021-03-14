dbfaa55d3e01853ec2e26c3dc0f742e7050ba1dc
Ankit Chouhan <ankit.chouhan@groww.in>
2021-03-14 18:23:18 +0530

    Example reset

--- example/sampleComponent.js
+++ example/sampleComponent.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Component = () => {
     return (
-        <div className="sc56Container">
-            <div className="sc56Header sc56Title">Header</div>
-            <div className="sc56Description sc56SubTitle">Desc</div>
+        <div className="Container">
+            <div className="Header Title">Header</div>
+            <div className="Description SubTitle">Desc</div>
         </div>
     );
 }
